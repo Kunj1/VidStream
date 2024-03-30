f17a1c5d8dcc28f27ba11b226631b9671d2d3f74
Kunj <kunj7dgupta@gmail.com>
2024-03-30 11:32:10 +0530

    Revert "Google-auth"
    This reverts commit 67a9eba3bfb38f127e1d30d0cb7e1b2f333ab349.

--- server/models/User.js
+++ server/models/User.js
@@ -13,6 +13,7 @@ const UserSchema = new mongoose.Schema({
     },
     password:{
         type: String,
+        required: true,
     },
     img:{
         type: String,
@@ -23,13 +24,8 @@ const UserSchema = new mongoose.Schema({
     },
     subscribedUsers:{
         type:[String],
-    },
-    fromGoogle: {
-        type: Boolean,
-        default: false,
-      },
-},
-    {timestamps:true}
+    }
+},{timestamps:true}
 );
 
 export default mongoose.model("User", UserSchema);
